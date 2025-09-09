50d3edb5592eb08a3539d8afe0adbb9209f2c4ed
Lorran Sutter <sutterlorran@gmail.com>
2025-09-09 00:01:36 -0400

    fix typo

--- src/core/constants/dimo.ts
+++ src/core/constants/dimo.ts
@@ -10,7 +10,7 @@ export const SolidityTypeAddress = "address";
 
 // used for pair aftermarket device type hash generation
 export const PairAftermarketDeviceSign = "PairAftermarketDeviceSign";
-export const VehicleNode = "VehicleNode";
+export const VehicleNode = "vehicleNode";
 
 export const REFUND_EVM_ADDRESS_PROD = "0x62b98e019e0d3e4A1Ad8C786202e09017Bd995e1";
 export const REFUND_EVM_ADDRESS_DEV = "0xC008EF40B0b42AAD7e34879EB024385024f753ea";
